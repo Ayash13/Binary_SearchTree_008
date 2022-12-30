184d638e05c103717d5c049c6246c6d786e3c86e
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-12-30 17:16:08 +0700

    Create class Node, then create variable and then define the variable

--- Binary_SearchTree_008/Program.cs
+++ Binary_SearchTree_008/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace Binary_SearchTree_008
 {
+
+    class Node
+    {
+        public string info;
+        public Node lefchild;
+        public Node rightchild;
+
+        // constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            lefchild = l;
+            rightchild = r;
+        }
+    }
+
+    // A node class consist as tree things, the information, the reference to the right child, and reference to the left child
+
+
     internal class Program
     {
         static void Main(string[] args)
